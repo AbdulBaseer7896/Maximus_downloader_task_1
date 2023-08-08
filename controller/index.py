e39af13544f629ba816134848f4e74eccef68937
Abdul <abdulbasirqazi7896@gmail.com>
2023-08-09 03:36:15 +0500

    NOw its work well.

--- controller/index.py
+++ controller/index.py
@@ -1,30 +1,33 @@
 from app import app
-from functools import wraps
-from flask import session
-from flask import redirect , url_for , render_template , request , flash , send_file
-import ast , os
+from flask import  render_template , request , send_file
+import  os
 from controller.yt_dlp_file import dlp
 
 obj = dlp
 
-@app.route('/index' , methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
 def download_fun():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print("This is form data" , data)
-        print("This is form data" , data['video_url'])
-        format = {'format': 'best'}
-        info = obj.download_video(f"{data['video_url']}" , format)
-        video_info = obj.get_video_info(f"{data['video_url']}" , format)
-        print("Video Name:", video_info)
+        print("This is form data", data)
+        print("This is form data", data['video_url'])
+        video_quality = request.form['video_quality']
         
-            # Check if the file exists and return it
+        try:
+            format = {'format': f'bestvideo[height<={video_quality}]+bestaudio/best[height<={video_quality}]'}
+            info = obj.download_video(data['video_url'], format)
+        except:
+            format = {'format': 'best'}
+            obj.download_video(data['video_url'], format)
+        video_info = obj.get_video_info(data['video_url'], format)
+        print("Video Name:", video_info)
+
+        # Check if the file exists and return it
         if os.path.exists(video_info):
-            new_filename = 'file_for_download.mp4'  # Specify the new filename you want
+            new_filename = 'your_download_file.mp4'  # Specify the new filename you want
             return send_file(video_info, as_attachment=True, download_name=new_filename)
-        # else:
-        #     return "File not found"
-        return render_template('download_fail_page.html' , download_link = "file_for_download.mp4")
+        return render_template('download_fail_page.html')
+
     
     
 
--- controller/yt_dlp_file.py
+++ controller/yt_dlp_file.py
@@ -12,12 +12,14 @@ class dlp:
             filename_template = ydl.prepare_filename(info_dict)
             return filename_template
 
-    def download_video(url,  ydl_opts):
+    @staticmethod
+    def download_video(url, ydl_opts):
+        ydl_opts['format'] = ydl_opts['format']  # Add this line to pass the format option
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
 
 obj = dlp 
 format = {'format': 'best'}
 
-obj.download_video('https://www.youtube.com/watch?v=hr3KIb_Buvk' , format)
+# obj.download_video('https://www.youtube.com/watch?v=hr3KIb_Buvk' , format)
 
